93acda622e18a538168050f75dcca4b42ec09020
Kevin Balke <kbalke@ucla.edu>
2016-06-04 21:59:55 -0700

    Fixed configure script

--- configure.py
+++ configure.py
@@ -25,26 +25,28 @@ with open("build.ninja", "w") as buildfile:
     n = Writer(buildfile)
 
     # Variable declarations
-    n.variable("tc_path", "/home/kbalke/Projects/Coding/energia-0101E0016/hardware/tools/lm4f/bin")
-    n.variable("cflags", "-g -c -Os -w -fno-rtti -fno-exceptions -ffunction-sections -fdata-sections -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -DF_CPU=80000000L -DPART_TM4C123GH6PM " + get_includes())
-    n.variable("lflags", "-g -Os -nostartfiles -nostdlib -Wl,--gc-sections -T lm4fcpp_blizzard.ld -Wl,--entry=ResetISR -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -lm -lc -lgcc")
+    n.variable("cflags", "-c -Os -w -fno-rtti -fno-exceptions -ffunction-sections -fdata-sections -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -DF_CPU=80000000L -DPART_TM4C123GH6PM " + get_includes())
+    n.variable("lflags", "-Os -nostartfiles -nostdlib -Wl,--gc-sections -T lm4fcpp_blizzard.ld -Wl,--entry=ResetISR -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -lm -lc -lgcc")
 
     # Rule declarations
     n.rule("cxx",
-           command = "$tc_path/arm-none-eabi-g++ $cxxflags -c $in -o $out")
+           command = "arm-none-eabi-g++ $cxxflags -c $in -o $out")
 
     n.rule("cc",
-           command = "$tc_path/arm-none-eabi-gcc $cflags -c $in -o $out")
- 
+           command = "arm-none-eabi-gcc $cflags -c $in -o $out")
+
+    n.rule("cca",
+           command = "arm-none-eabi-gcc $cflags -S -c $in -o $out")
+
     n.rule("cl",
-           command = "$tc_path/arm-none-eabi-gcc $lflags $in -o $out")
- 
+           command = "arm-none-eabi-gcc $lflags $in -o $out")
+
     n.rule("oc",
-           command = "$tc_path/arm-none-eabi-objcopy -O binary $in $out")
- 
+           command = "arm-none-eabi-objcopy -O binary $in $out")
+
     n.rule("cdb",
            command = "ninja -t compdb cc > compile_commands.json")
- 
+
     n.rule("cscdb",
            command = "cscope -Rbq")
 
@@ -58,6 +60,10 @@ with open("build.ninja", "w") as buildfile:
         ofile = subst_ext(name, ".o")
         n.build(ofile, "cc", name)
         objects.append(ofile)
+    def cca(name):
+        ofile = subst_ext(name, ".s")
+        n.build(ofile, "cca", name)
+        objects.append(ofile)
     def cxx(name):
         ofile = subst_ext(name, ".o")
         n.build(ofile, "cxx", name)
@@ -67,7 +73,7 @@ with open("build.ninja", "w") as buildfile:
 
     sources = get_sources()
     map(cc, filter(lambda x : x.endswith(".c"), sources))
-    map(cxx, filter(lambda x : x.endswith(".cxx"), sources))
+    map(cxx, filter(lambda x : x.endswith(".cpp"), sources))
 
     cl("main.elf", objects)
 
